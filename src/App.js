b59d83267213f25bb542e77cfb84ce78835e10c9
SuryaPrasadh <87569115+SuryaPrasadh@users.noreply.github.com>
2021-07-20 01:27:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route exact path=".reactdeploy/">
+            <Route exact path="https://suryaprasadh.github.io/reactdeploy/">
               <Home />   
             </Route>
             <Route path="/create">
